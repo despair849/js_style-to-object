cbf13e139eb7edad8b46a5ee7b3d743ed0e61a27
Yurii Drutsul <druculurij@gmail.com>
2025-10-19 17:43:59 +0300

    message

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,20 +1,21 @@
 'use strict';
 
-'use strict';
-
 function convertToObject(sourceString) {
   return sourceString
     .split(';')
     .map((line) => line.trim())
     .filter((line) => line)
-    .reduce((result, line) => {
+    .reduce((stylesMap, line) => {
       const [key, value] = line.split(':');
 
       if (key && value) {
-        result[key.trim()] = value.trim();
+        return {
+          ...stylesMap,
+          [key.trim()]: value.trim(),
+        };
       }
 
-      return result;
+      return stylesMap;
     }, {});
 }
 
